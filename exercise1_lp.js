35f7504d10c00c42641798ab49218e0745a94c05
Henriquedev1 <132936272+Henriquedev1@users.noreply.github.com>
2023-10-19 18:25:18 -0300

    Add files via upload
    primeiro commit

---
+++ exercise1_lp.js
@@ -0,0 +1,8 @@
+//Escreva um programa que mostre todos os múltiplos de 3 de 0 até 10000.
+
+for (let i = 0; i < 10000;) {
+  if (i % 3 == 0) {
+    console.log (`${i} é um multiplo de 3`) 
+  }
+  i = i +1
+}  
